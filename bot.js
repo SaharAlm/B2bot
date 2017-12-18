aa294144a61c5936f6826f166423b95d9cfea336
Twins1 <33881658+Twins1@users.noreply.github.com>
2017-12-18 17:36:53 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,69 @@
+const Discord = require("discord.js");
+const token = 'MzkxNTU2MzkxODM3NDk5Mzky.DRatlg.DvM7NLPDi50svzPnyVEdqM6kTc0';
+const prefix = '#';
+
+var bot = new Discord.Client();
+
+bot.on("ready", () => {
+  console.log('online!')
+
+bot.user.setGame('B2ForEver - beta')
+});
+
+bot.on("guildMemberAdd", member => {
+
+  if (member.guild.id === '391521978634469386')
+  member.guild.channels.get('391525603192602634').send(`**${member.user.username}** Has joined to the Team! Conagratz`)
+});
+
+bot.on("guildMemberRemove", member => {
+  member.guild.channels.get('391525603192602634').send(`**${member.user.username}** Has left the Team! Bye Bye `)
+});
+
+bot.on("message", msg => {
+
+  if (msg.content.startsWith(prefix + 'ServerInfo')) {
+    msg.channel.send({embed:{
+      title: 'ServerInfo',
+      description: 'The is offical server of the Team!',
+      color: 0xFFD800
+    }});
+  }
+
+   if (msg.content.startsWith(prefix +'TeamInfo')) {
+     msg.channel.send({embed:{
+       title: 'TeamInfo',
+       description: 'B2 is an Official Gaming Team!',
+       color: 0xFFD800
+     }});
+   }
+
+   if (msg.content.startsWith(prefix + 'RL')) {
+     msg.member.addRole('391639199755010059')
+   }
+   if (msg.content.startsWith(prefix +'PUBG'))  {
+     msg.member.addRole('391639687464222721')
+   }
+
+   if (msg.content.startsWith(prefix + 'OW'))  {
+     msg.member.addRole('391639844217946113')
+   }
+
+   if (msg.content.startsWith(prefix + 'CSGO')) {
+     msg.member.addRole('391639455884115978')
+   }
+
+   if (msg.content.startsWith(prefix + 'sss')) {
+     msg.member.addRole('391524702000119829')
+   }
+
+   if (msg.content.startsWith(prefix + 'steam')) {
+     msg.reply('http://steamcommunity.com/groups/BtOfficialTeam')
+   }
+
+   if (msg.content.startsWith(prefix + 'invite')) {
+     msg.channel.send('טופס קבלה לטים: https://goo.gl/forms/fZnx2O98Nupa0Fbh2')
+   }
+});
+
+bot.login(token)
