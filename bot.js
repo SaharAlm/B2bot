573f85af651855fa1ba2fb7a98e6f35ae76708f7
Twins1 <33881658+Twins1@users.noreply.github.com>
2017-12-22 14:03:12 +0200

    גדגדג

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ bot.on("message", msg => {
    }
 
    if (msg.content.startsWith(prefix + 'help')) {
-     msg.channel.send(`<@&391522223992733696> צריכים עזרה! `)
+     msg.channel.send(`<@&391548502964174851> ${member.user.username} צריך עזרה `)
    }
 
    if (msg.content.startsWith(prefix + 'ssss')) { //למקרה ו...
