da7a096c13ef626256304dbf94dc597b2b208728
Twins1 <33881658+Twins1@users.noreply.github.com>
2017-12-23 10:21:05 +0200

    dsdss

--- bot.js
+++ bot.js
@@ -14,11 +14,11 @@ bot.user.setGame('B2ForEver - beta')
 
 bot.on("guildMemberAdd", member => {
   if (member.guild.id === '391521978634469386')
-  member.guild.channels.get('391525603192602634').send(`<${member.user.username}> Has joined to the Team! Conagratz`)
+  member.guild.channels.get('391525603192602634').send(`**${member.user.username}** Has joined to the Team! Conagratz`)
 });
 
 bot.on("guildMemberRemove", (member) => {
-  member.guild.channels.get('391525603192602634').send(`<${member.user.username}> Has left the Team! Bye Bye `)
+  member.guild.channels.get('391525603192602634').send(`**${member.user.username}** Has left the Team! Bye Bye `)
 });
 
 
